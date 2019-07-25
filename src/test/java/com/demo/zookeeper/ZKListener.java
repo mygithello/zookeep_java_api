1a02c03eefea50c9d3c8c9faa300b19a55b06d7e
jyc <1357602588@qq.com>
2019-07-25 15:07:11 +0800

    ----补充

--- src/test/java/com/demo/zookeeper/ZKListener.java
+++ src/test/java/com/demo/zookeeper/ZKListener.java
@@ -4,6 +4,15 @@ import org.I0Itec.zkclient.IZkDataListener;
 import org.I0Itec.zkclient.ZkClient;
 import java.io.IOException;
 import java.util.List;
+
+/**
+ * 注册监听
+ * 在ZkClient中客户端可以通过注册相关的事件监听来实现对Zookeeper服务端事件的订阅，其中
+ * ZkClient提供的常⽤用监听事件接⼝口有以下⼏几种：
+ * 监听接⼝口               订阅⽅方法       取消订阅⽅方法
+ * IZkChildListener subscribeChildChanges unsubscribeChildChanges
+ * IZkDataListener subscribeDataChanges unsubscribeDataChanges
+ */
 public class ZKListener {
     public static void main(String[] args) throws IOException {
         ZkClient zkClient = new ZkClient("192.168.42.132:2181");
